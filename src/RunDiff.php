bb1242ac37a217af5b24803cd85ae9544fea6a13
VdovinAV <aleksandr.vdovin@vitech.team>
2023-08-23 17:16:30 +0300

    draft

--- src/RunDiff.php
+++ src/RunDiff.php
@@ -71,82 +71,19 @@ class RunDiff extends Extension
 
     public function build()
     {
-//        $commitHash = exec('git rev-parse HEAD');
-//        $diff = shell_exec("git diff --name-only master $commitHash -- $filePath");
-//        echo $diff;
-        // Get the changed files between the current branch and the master branch
-        exec('git diff --name-only master...HEAD', $changed_files);
-        foreach ($changed_files as $diff) {
-            exec('git diff master... ' . $diff, $output);
-print_r($output) ;
 
-// Parse each file and extract the changed functions
-//foreach ($files as $file) {
-//    if (!empty($file)) {
-//        $code = file_get_contents($file);
-//
-//        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
-//        $traverser = new NodeTraverser;
-//        $traverser->addVisitor(new NameResolver);
-//
-//        try {
-//            $stmts = $parser->parse($code);
-//            $stmts = $traverser->traverse($stmts);
-//
-//            // Extract the changed functions
-//            foreach ($stmts as $stmt) {
-//                if ($stmt instanceof \PhpParser\Node\Stmt\Function_) {
-//                    echo 'Changed function: ' . $stmt->name->name . PHP_EOL;
-//                }
-//            }
-//        } catch (Error $error) {
-//            echo 'Error parsing file: ' . $file . PHP_EOL;
-//        }
-//    }
+// Get all files in the current branch that have changes compared to the master branch
+        $modifiedFiles = shell_exec("git diff --name-only origin/master");
+
+// Filter the modified files to keep only those whose name ends with "Cest"
+        $modifiedFiles = array_filter(explode("\n", $modifiedFiles), function ($file) {
+            return substr($file, -4) === 'Cest';
+        });
+
+// Display the modified files
+        foreach ($modifiedFiles as $modifiedFile) {
+            echo $modifiedFile . "\n";
         }
 
-//
-//        // Iterate over each changed file
-//        foreach ($changed_files as $file) {
-//            // Check if the file is a PHP file
-//            if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
-//                // Get the diff of the changes in the file for the specified commit hash
-//                exec("git diff " . $commitHash . "..HEAD -- " . $file, $diff);
-//                print_r($diff);
-//                echo '_______________' . "\n";
-//                // Iterate over each line in the diff
-//                foreach ($diff as $line) {
-//                    // Check if the line contains a function declaration
-//                    if (preg_match('/^\+.*function\s+(\w+)\(/', $line, $matches)) {
-//                        $function_name = $matches[1];
-//                        $full_path = realpath($file);
-//                        echo "Function: {$function_name}\n";
-//                        echo "Path: {$full_path}\n\n";
-//                    }
-//                }
-//            }
-//        }
     }
-
-
-//    public function saveFailed(PrintResultEvent $event): void
-//    {
-//        $file = $this->getLogDir() . $this->group;
-//        $output = [];
-//            $output[] = $this->localizePath(Descriptor::getTestFullName($);
-//        foreach ($result->errors() as $fail) {
-//            $output[] = $this->localizePath(Descriptor::getTestFullName($fail->getTest()));
-//        }
-//
-//        file_put_contents($file, implode("\n", $output));
-//    }
-//
-//    protected function localizePath(string $path): string
-//    {
-//        $root = realpath($this->getRootDir()) . DIRECTORY_SEPARATOR;
-//        if (substr($path, 0, strlen($root)) === $root) {
-//            return substr($path, strlen($root));
-//        }
-//        return $path;
-//    }
-    }
+}
